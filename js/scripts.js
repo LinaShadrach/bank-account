3c37ce67ce539e53f1399fd8ad3e7c5e6fd48a91
Erik Killops and Lina Shadrach <lina@>
2016-11-01 11:45:15 -0700

    create constructors for client and account. display name and balance on submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,27 @@
+//Back End
+function Client(clientName, password, accounts) {
+  this.clientName = clientName;
+  this.password = password;
+  this.accounts = [];
+}
+
+function Account(accountName, accountType, balance, interest) {
+  this.accountName = accountName;
+  this.accountType = accountType;
+  this.balance = balance;
+  this.interest = interest;
+}
+
+
+
+//Front End
+
 function checkInputType(deposit) {
   var nonDigits = /[\D]/g
   if (deposit.match(nonDigits)){
     alert("that's not a number.");
   }
 }
-
-
 
 
 $(document).ready(function(){
@@ -19,9 +35,12 @@ $(document).ready(function(){
     var inputPassword = $("#password").val();
     var inputDeposit = $("#deposit").val();
     checkInputType(inputDeposit);
+
+    var account = new Account("First Account", "savings", parseFloat(inputDeposit), "0%");
+    var client = new Client(inputClientName, inputPassword, account);
     $("#clientInfo").show();
     $("#nameDisplay").text(inputClientName);
-    $("#accounts").append("<li> Balance: " + inputDeposit + "</li>");
+    $("#accounts").append("<li> Balance: " + account.balance + "</li>");
   });
 
 });
